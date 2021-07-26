e97d6d680ecd9791e11edf5b8841bd34c666dac4
VinnieIT <mwangivin3@gmail.com>
2021-07-26 13:16:13 +0300

    Repos class

---
+++ src/app/repos.ts
@@ -0,0 +1,18 @@
+export class Repo {
+    public ID: number;
+    public name: string;
+    public descr: string;
+    public creationDate: string;
+    public lang: string
+
+    constructor(
+        
+    ){
+        this.ID = 0;
+        this.name = "";
+        this.descr = "";
+        this.creationDate = "";
+        this.lang = "";
+    }
+}
+
