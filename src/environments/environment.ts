f5519294c45aa7cae581fac98d3ff6a9a628881e
VinnieIT <mwangivin3@gmail.com>
2021-07-27 18:06:36 +0300

    Add the api key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: "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"
 };
 
 /*
