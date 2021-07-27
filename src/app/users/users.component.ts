c676675ca2fd717a4147e724261dcc4db9edd3b0
VinnieIT <mwangivin3@gmail.com>
2021-07-27 18:04:38 +0300

    Add the user component

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,13 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Users } from '../users'; 
+import { Repo } from '../repos';
+import {ApiResponseServService} from '../api-response-serv.service'
+
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.css']
+  styleUrls: ['./users.component.css'],
+  providers:[ApiResponseServService]
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+  constructor(apiRespond:ApiResponseServService, private http:HttpClient) { }
 
   ngOnInit(): void {
   }
