769e344973fddbcb7747bbf88efac87d40576c5c
VinnieIT <mwangivin3@gmail.com>
2021-07-29 10:43:01 +0300

    Add path for the about us page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Users } from './users';
 import { UsersComponent } from './users/users.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 const routes: Routes = [
-  {path: 'Users', component: UsersComponent},
+  {path:'about-us', component: AboutUsComponent},
   
 ];
 
